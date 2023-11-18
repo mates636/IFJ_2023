f8d144ff48fe07fd13503377af1aecb668623c5f
MikiU11 <95191052+MikiU11@users.noreply.github.com>
2023-11-18 17:15:20 +0100

    Update parser.h

--- parser.h
+++ parser.h
@@ -19,15 +19,12 @@ error_t parser_argument(scanner_t *scanner, token_t *token);
 error_t parser_variable(scanner_t *scanner, token_t *t);
 error_t parser_variable_identifier(scanner_t *scanner, token_t *token, bool can_modify);
 error_t parser_variable_type_and_data(scanner_t *scanner, token_t *token, bst_node *tree_node);
-<<<<<<< HEAD
-error_t parser_expression(scanner_t *scanner, token_t *token);
+error_t parser_expression(scanner_t *scanner, token_t *token, variable_type *control_type);
 error_t parser_function(scanner_t *scanner, token_t *token);
 error_t parser_return_type(scanner_t *scanner, token_t *token);
 
-=======
-error_t parser_expression(scanner_t *scanner, token_t *token, variable_type *control_type);
+
 //error_t parser_function();
->>>>>>> f870b633c4cc836d7be7edae4cf125bba5f68499
 //error_t parser_if_statement();
 //error_t parser_while_statement();
 
