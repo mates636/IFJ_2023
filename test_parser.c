91e9fe20f47a6acb0b87c595f199a3ef0b68965f
MikiU11 <95191052+MikiU11@users.noreply.github.com>
2023-11-18 17:17:24 +0100

    Update test_parser.c

--- test_parser.c
+++ test_parser.c
@@ -14,12 +14,6 @@ int main(int argc, char** argv){
     FILE* source = fopen(argv[1], "r");
     scanner_t* scanner = init_scanner(source);
     init_parser();
-<<<<<<< HEAD
-    error_t error = run_parser(scanner);
-    printf("%d\n", error);
-    return 0;
-}
-=======
     run_parser(scanner);
     free_parser();
     fclose(source);
