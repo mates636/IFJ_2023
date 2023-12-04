758477eee7cf01ad00c7d1529d9231777d1db300
Yetiia <128994948+Yetiia@users.noreply.github.com>
2023-12-04 01:10:51 +0100

    Update stack_token.h

--- stack_token.h
+++ stack_token.h
@@ -1,5 +1,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#iclude "scanner.h"
+
+#define LX_ERROR 1
+#define IT_ERROR 99
 
 typedef struct stack_item stack_item_t;
 struct stack_item{
@@ -7,7 +11,7 @@ struct stack_item{
   int value_item;
 }
 typedef struct int_stack_item{
-  stack_item_t* top;
+  stack_item_t* top_item;
   int count;
 }int_stack_item_t
 
@@ -51,8 +55,43 @@ bool Count_stack(int_stack_item_t* stack, int* value_item);
 */
 bool Offset_stack(int_stack_item_t* stack, int* value_item, int num);
 
+typedef struct token_item_stack token_item_stack_t;
+struct token_item_stack{
+  token_item_stack_t* next_token;
+  token_t value_token;
+}
+typedef struct token_stack{
+  token_item_stack* top_token;
+  FILE* file;
+  int_stack_item_t* lx_stack;
+}token_stack_t;
 
+/**
+* Inicializace stacku tokenu
+*/
+token_stack_t* Init_token_stack(FILE* file, int_stack_item_t* lx_stack);
 
+/**
+* Uvolneni stacku tokenu
+*/
+void Free_token_stack(token_stack_t* stack);
 
+/**
+* Kontrola jestli je stack tokenu prazdny
+*/
+bool Empty_token_stack(token_stack_t* stack);
 
+/**
+* Push pointr
+*/
+void Push_token_stack(token_stack_t* stack, token_t value_token);
 
+/**
+* Pop item ze stacku, pokud je stack prazdny bereme nasledujici token
+*/
+token_t Pop_token_stack(token_stack_t* stack, int* err);
+
+/**
+* Top item ze stacku, pokud je stack prazdny bereme nasledujici token
+*/
+token_t Top_token_stack(token_stack_t* stack, int* err);
